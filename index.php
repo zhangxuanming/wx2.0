474b5b8b9e6375613aa3c98a2a7b8b7e849f5efb
bor <muyuruhai@gmail.com>
2015-10-15 22:17:40 +1100

    游戏区绘制

--- index.php
+++ index.php
@@ -14,10 +14,14 @@
     <!-- Bootstrap -->
     <link href="css/bootstrap-3.3.5-dist/css/bootstrap.css" rel="stylesheet">
 	<!-- zihi style sheet-->
-    <link href="css/app.css" rel="stylesheet">
+<!--    <link href="css/app.css" rel="stylesheet">-->
 	<link href="css/wx20.css" rel="stylesheet">
-	<!-- template engine	-->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/greensock-js/src/minified/TweenMax.min.js"></script>
+	<script src="js/underscore-min.js"></script>
     <script src="js/juicer-min.js"></script>
+	<!-- template engine	-->
+	<script src="css/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
 	<!-- <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>-->
 
 	<img src="src/img/head.jpg" style="position:absolute;width: 0;height: 0;">
@@ -60,93 +64,41 @@
     </script>
 </head>
 
-<body class="zh-black" ontouchstart="">
-<div class="container" style="overflow-x: hidden">
+<body class="" ontouchstart="">
+<div class="container full" style="overflow-x: hidden">
 <!--	上个用户页面-->
-	<div class="row page pagelastuser full zh-hidden zh-yellow">
-		<div class="col-md-12 full">
-			<div class="row">
-				<!--徽章-->
-				<div class="col-xs-12" style="background-color: #ffffff">
-					<div id="lastuserbadge" class="zh-stamp center-block"
-					     style="background-image: url(./src/img/badge/angry.png);"></div>
-				</div>
-				<div id="lastusersummary" class="col-xs-12">
-					<h2 id="lastusername" style="font-weight: bold"></h2>
-					<h4>在字嗨逃离深山 大冒险中</h4>
-					<h4>获得称号</h4>
-					<h3 id="lastuserdes"></h3>
-				</div>
-			</div>
-		</div>
-		<div class="col-xs-12" style="position: absolute;bottom: 0;left: 0">
-			<div class="row" >
-				<div class="col-xs-12">
-					<button data-from="0" data-to="1"
-					        class="btn btn-block zh-btnwant"
-					        style="font-weight: bold;">我也要玩</button>
-				</div>
-			</div>
-		</div>
-	</div>
+
 <!--	splash页面-->
-	<div class="row page pagesplash full">
+	<div class="row page pagesplash full zh-hidden">
 		<div class="col-xs-12 v-center ">
 			<img class="splash-logo" src="src/img/splash/bg2.png" />
 		</div>
 	</div>
-<!--	起始页-->
-    <div class="row page pagestart full zh-hidden zh-yellow" style="background:url(../src/img/yure1/startbg.png);">
-	    <div class="col-xs-12 ps-block">
-		    <h3 class="center-block text-center zh-t-white"
-		        style="float: left;margin-left: 5%;font-size: 1.2em;color: rgba(255, 255, 255, 0.78);"
-			    >嗨！冒险 之</h3>
-		    <h1 class="center-block text-center zh-t-white"
-		        style="float: left;margin-left: 5%;clear: both;margin-top: 5px;"
-			    >逃离深山</h1>
-		    <h3 class="center-block zh-t-white"
-		        style="float: left;text-align: left;margin:5%;font-size: 1.2em;color: rgba(255, 255, 255, 0.78);line-height: 1.5em;"
-			    >你从酒吧出来已是午夜，夜灯下无人的马路自有其浪漫风味。忽然你觉得脑后一疼……</h3>
-		    <div class="row zh-name-wrap">
-			    <input id="zh-name" class="zh-name" type="text" maxlength="8" value="你的名字?" placeholder="默认嗨客">
-		    </div>
+
+<!--    游戏页-->
+    <div class="row wrap full zh-yellow">
+<!--	    头部信息-->
+	    <div class="col-sm-12 g-top">
+
+	    </div>
+<!--	    游戏区-->
+	    <div class="col-sm-12 g-middle">
 		    <div class="row">
-			    <button data-from="0" data-to="1"
-			            class="col-xs-10 col-xs-offset-1 btn btn-lg zh-btn zh-btn-yellow zh-btnstart"
-			            style="font-weight: bold;"
-				    >开始嗨</button>
+			    <div class="col-sm-12 g-info" style="background-color: greenyellow">12321</div>
+			    <div class="col-sm-12 g-area">
+				    <div class="g-wrap">
+
+				    </div>
+			    </div>
 		    </div>
 	    </div>
+<!--	    底部操作区-->
+	    <div class="col-sm-12 g-bottom"></div>
     </div>
-
-    <!--剧情会被插入这里-->
 
     <!--结局页-->
     <div class="row page pageend full zh-hidden zh-yellow" style="background-color: #ffe681">
-	    <div class="col-xs-12" style="margin-top: -6%;">
-		    <div class="row"><!--徽章-->
-			    <div class="col-xs-12" style="background:#fff;"><!--分值-->
-				    <p id="zh-summaryPage-title" class="text-center" style="padding-top: 10%;color: #777;">根据您的战斗表现，我们觉得您的战斗力为 :</p>
-				    <p class="text-center">
-					    <span class="zh-totalscore text-center" style="font-size:60pt;color:#FFF054;line-height: 1"></span>
-				    </p>
-				    <p class="text-center zh-t-yellow1">战斗力</p>
-				    <!--结语-->
-				    <img class="zh-stamp center-block" style="margin-top: -3px;margin-bottom: 4%;" src="./src/img/badge/stamp_3.png" />
-			    </div>
-		    </div>
-	    </div>
-	    <div class="col-xs-12 v-bottom" style="background-color: #ffe681">
-		    <div class="row">
-			    <div class="col-xs-12" style="margin-top: 2%;">
-				    <br>
-				    <h4 class="zh-summarytext text-center" style="    font-size: 24px;margin-top: 5%;color: #8A6E00;"></h4>
-			    </div>
-			    <div class="col-xs-6 col-xs-offset-3">
-				    <button class="btn btn-block btn-lg zh-btn zh-restartbtn">重玩一次</button>
-			    </div>
-		    </div>
-	    </div>
+
     </div>
 
 </div>
@@ -183,17 +135,18 @@
 	</div>
 </div>
 
-<script src="js/jquery-2.1.4.min.js"></script>
 <!--配置页面-->
-<script src="js/config.js"></script>
-<script src="css/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
-<script src="js/greensock-js/src/minified/TweenMax.min.js"></script>
-<script src="js/zh-game.js"></script>
-<script src="js/zh-story.js"></script>
+<script src="js/zh-gameLayout.js"></script>
+<!--<script src="js/config.js"></script>-->
+<!--<script src="js/zh-game.js"></script>-->
+<!--<script src="js/zh-story.js"></script>-->
 <!--<script src="js/hammer/hammer.js"></script>-->
 
 <script>
 
+	$(document).ready(function(){
+		gameModule.init();
+	});
 </script>
 </body>
 </html>
---
+++ js/zh-gameLayout.js
@@ -0,0 +1,109 @@
+/**
+ * Created by muyuruhai on 15/10/15.
+ */
+
+var gameModule = (function(my){
+
+    my.init = function(){
+        this.Layout.update();
+    };
+    //刷新布局
+    my.updateLayout = function(layoutConfig){
+        this.Layout.update(layoutConfig);
+    };
+    return my;
+}(gameModule || {}));
+
+//布局子模块
+gameModule.Layout = (function(my){
+    var $gArea = $('.g-area')
+        ,$gWrap = $('.g-wrap');
+    var _layoutConfig = {
+        col : 6,
+        row : 5,
+        margin :3
+    };
+    var setlayoutConfig  = function(lc){
+        _layoutConfig = lc || _layoutConfig;
+    };
+    //取得包围框的大小
+    var getWrapSize = function(){
+        var _w = $gArea.width();
+        _w = _w - _w % _layoutConfig.col;
+        return {
+            width  : _w,
+            height : _w
+        }
+    };
+    //设定wrap大小
+    var setWrapSize = function(w,h){
+        $gWrap.width(w);
+        $gWrap.height(h);
+    };
+
+    //计算每个按键大小返回size Object
+    var getSizes = function(){
+        var _wrapSize = getWrapSize();
+        var _w = 0
+            ,_h = 0;
+        _w = Math.floor((_wrapSize.width / _layoutConfig.col) - _layoutConfig.margin);
+        _h = _w;
+        return {
+            wrapWidth : _w * _layoutConfig.col + _layoutConfig.margin*(_layoutConfig.col-1),
+            wrapHeight : _h * _layoutConfig.row + _layoutConfig.margin * (_layoutConfig.row+1),
+            blockWidth  :_w,
+            blockHeight :_h
+        }
+    };
+
+    //根据size 计算布局确定坐标
+    var getBlockPostion = function(sizeObj){
+        var _blockSize = sizeObj
+            ,_blockQty = _layoutConfig.col * _layoutConfig.row;
+        var _poObj = [];
+        var offsetX = 0
+            ,offsetY = 0;
+        var _x = offsetX
+            ,_y= offsetY
+            ,_n=0;
+        for (var j = 0; j<_layoutConfig.row;j++){
+            _y = _y + _layoutConfig.margin;
+            for (var i = 0;i<_layoutConfig.col;i++){
+                _poObj[_n] = {
+                    x :_x,y :_y,
+                    bx:offsetX,by:offsetY
+                };
+                if (i!==_layoutConfig.col-1){
+                    _x = _x + _layoutConfig.margin;
+                }
+                _x = _x + _blockSize.blockWidth;
+                _n++;
+            }
+            _x = offsetX;
+            _y = _y + _blockSize.blockHeight ;
+        }
+        return _poObj;
+    };
+
+    //根据坐标渲染布局，这里需要接入以后的文字逻辑
+    var randerLayout = function(){
+        var sizeObj = getSizes();
+        var positions = getBlockPostion(sizeObj);
+        var gb = "";
+        $.each(positions,function(i,v){
+            gb += '<button data-index="'+i+'" class="g-block" style="width:'+sizeObj.blockWidth+'px;height:'+sizeObj.blockHeight+'px;top:'+v.y+'px;left:'+ v.x+'px">'+i+'</button>';
+        });
+        setWrapSize(sizeObj.wrapWidth,sizeObj.wrapHeight);
+
+        $gWrap.empty().append(gb);
+    };
+
+    //my.refreshLayout = function(){
+    //    randerLayout();
+    //};
+    my.update = function(layoutConfig){
+        setlayoutConfig(layoutConfig);
+        randerLayout();
+    };
+    return my;
+}(gameModule.Layout || {}));
