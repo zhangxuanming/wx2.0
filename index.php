7ef0d5b9fc55f811090ae74aedc6cf51052dc2a2
bor <muyuruhai@gmail.com>
2015-11-03 21:52:13 +1100

    模块应该OK了

--- index.php
+++ index.php
@@ -392,9 +392,10 @@ console.log(navigator);
 					var mox = ($(".g-cardWrap").size()-1) * -(card.width()/2);//15;
 					var moy = 0; //($(".g-cardWrap").size()-1) * 3;
 					tl.fromTo(card,0.5,{alpha:0,scale:0},{alpha:1,scale:1,'zIndex':'1',ease:Back.easeInOut})
-						.to(card,0.5,{'left':bagSize.left,'top':h,ease:Back.easeOut},"+=0.2")
-						.to(card,0.5,{'top':mt,'left':ml,'height':mh,'boxShadow':'none',rotationY:0,x:mox,y:moy,ease:Back.easeInOut},"+=0.1")
-						.to(card,2,{alpha:2,"margin":0,'zIndex':'1',ease:Back.easeInOut});
+						.to(card,0.5,{'left':bagSize.left,'top':h,'zIndex':'1',ease:Linear.easeIn},"+=0.3")
+						.to(card,0.5,{'height':mh,rotation:360,'zIndex':'1',ease:Back.easeOut},"+=0.3")
+						.to(card,0.5,{'top':mt,'left':ml,'boxShadow':'none',x:mox,y:moy,'zIndex':'1',ease:Back.easeInOut},"+=0.1");
+//						.to(card,2,{alpha:2,"margin":0,'zIndex':'1',ease:Back.easeInOut});
 //						.to(card,0.5,{'top':mt,'left':ml,'height':mh,'width':mw,'boxShadow':'none',rotationY:-80,x:mox,y:moy,ease:Back.easeInOut});
 					boxTop = boxTop+dw;
 				}
